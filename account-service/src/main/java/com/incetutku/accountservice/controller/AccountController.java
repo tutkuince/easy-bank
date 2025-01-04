4d20a7cd31553f18ac87a6d55318afe465308b5c
Tutku Ince <tutku.ince@outlook.com>
2025-01-04 18:52:13 +0300

    Controller

--- account-service/src/main/java/com/incetutku/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/incetutku/accountservice/controller/AccountController.java
@@ -1,13 +1,21 @@
 package com.incetutku.accountservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.incetutku.accountservice.constants.AccountConstants;
+import com.incetutku.accountservice.dto.CustomerDto;
+import com.incetutku.accountservice.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api/v1/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountController {
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello World!";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 }
