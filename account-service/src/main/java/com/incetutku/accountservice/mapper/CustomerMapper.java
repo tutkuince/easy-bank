331dd2054a1e46186a98b6db2d2c0f8b499b98c0
Tutku Ince <tutku.ince@outlook.com>
2025-01-06 19:36:21 +0300

    wrong package has been changed

--- account-service/src/main/java/com/incetutku/accountservice/mapper/CustomerMapper.java
+++ account-service/src/main/java/com/incetutku/accountservice/mapper/CustomerMapper.java
@@ -1,18 +1,20 @@
-package com.incetutku.accountservice.controller;
+package com.incetutku.accountservice.mapper;
 
 import com.incetutku.accountservice.dto.CustomerDto;
 import com.incetutku.accountservice.entity.Customer;
 
 public class CustomerMapper {
 
-    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+    public static CustomerDto mapToCustomerDto(Customer customer) {
+        CustomerDto customerDto = new CustomerDto();
         customerDto.setName(customer.getName());
         customerDto.setEmail(customer.getEmail());
         customerDto.setMobileNumber(customer.getMobileNumber());
         return customerDto;
     }
 
-    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+    public static Customer mapToCustomer(CustomerDto customerDto) {
+        Customer customer = new Customer();
         customer.setName(customerDto.getName());
         customer.setEmail(customerDto.getEmail());
         customer.setMobileNumber(customerDto.getMobileNumber());
