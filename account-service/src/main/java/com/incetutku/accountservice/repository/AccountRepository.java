cf15e7929feae78d5e7e9a907a6b88cc790374dd
Tutku Ince <tutku.ince@outlook.com>
2025-01-06 20:45:47 +0300

    findCustomerByID

--- account-service/src/main/java/com/incetutku/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/incetutku/accountservice/repository/AccountRepository.java
@@ -3,5 +3,9 @@ package com.incetutku.accountservice.repository;
 import com.incetutku.accountservice.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByCustomerId(Long customerId);
 }
