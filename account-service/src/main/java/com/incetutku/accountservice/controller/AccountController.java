ae81f1bc832afb085d902045f59a428810bb952f
Tutku Ince <tutku.ince@outlook.com>
2025-01-06 20:46:35 +0300

    fetchAccountDetailsByMobileNumber

--- account-service/src/main/java/com/incetutku/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/incetutku/accountservice/controller/AccountController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -26,5 +23,10 @@ public class AccountController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
+    }
+
+    @GetMapping
+    public ResponseEntity<CustomerDto> fetchAccountDetailsByMobileNumber(@RequestParam String mobileNumber) {
+        return ResponseEntity.ok(accountService.fetchAccountByMobileNumber(mobileNumber));
     }
 }
