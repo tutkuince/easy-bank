1c7122f54e4155b815ea5c5a690b1f44def4d52f
Tutku Ince <tutku.ince@outlook.com>
2025-01-07 17:41:48 +0300

    Mappers have been updated

--- account-service/src/main/java/com/incetutku/accountservice/mapper/AccountMapper.java
+++ account-service/src/main/java/com/incetutku/accountservice/mapper/AccountMapper.java
@@ -5,16 +5,14 @@ import com.incetutku.accountservice.entity.Account;
 
 public class AccountMapper {
 
-    public static AccountDto mapToAccountDto(Account account) {
-        AccountDto accountDto = new AccountDto();
+    public static AccountDto mapToAccountDto(Account account, AccountDto accountDto) {
         accountDto.setAccountNumber(account.getAccountNumber());
         accountDto.setAccountType(account.getAccountType());
         accountDto.setBranchAddress(account.getBranchAddress());
         return accountDto;
     }
 
-    public static Account mapToAccount(AccountDto accountDto) {
-        Account account = new Account();
+    public static Account mapToAccount(AccountDto accountDto, Account account) {
         account.setAccountNumber(accountDto.getAccountNumber());
         account.setAccountType(accountDto.getAccountType());
         account.setBranchAddress(accountDto.getBranchAddress());
--- account-service/src/main/java/com/incetutku/accountservice/mapper/CustomerMapper.java
+++ account-service/src/main/java/com/incetutku/accountservice/mapper/CustomerMapper.java
@@ -5,16 +5,14 @@ import com.incetutku.accountservice.entity.Customer;
 
 public class CustomerMapper {
 
-    public static CustomerDto mapToCustomerDto(Customer customer) {
-        CustomerDto customerDto = new CustomerDto();
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
         customerDto.setName(customer.getName());
         customerDto.setEmail(customer.getEmail());
         customerDto.setMobileNumber(customer.getMobileNumber());
         return customerDto;
     }
 
-    public static Customer mapToCustomer(CustomerDto customerDto) {
-        Customer customer = new Customer();
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
         customer.setName(customerDto.getName());
         customer.setEmail(customerDto.getEmail());
         customer.setMobileNumber(customerDto.getMobileNumber());
