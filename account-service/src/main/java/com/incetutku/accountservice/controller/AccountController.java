04f1c02ee67c82f3f92b093e4185ca8bbbb3cbf0
Tutku Ince <tutku.ince@outlook.com>
2025-01-07 17:45:23 +0300

    update api

--- account-service/src/main/java/com/incetutku/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/incetutku/accountservice/controller/AccountController.java
@@ -25,6 +25,16 @@ public class AccountController {
                 .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+        boolean isUpdated = accountService.updateAccount(customerDto);
+        if (isUpdated) {
+            return ResponseEntity.ok(new ResponseDto(AccountConstants.STATUS_200, AccountConstants.MESSAGE_200));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseDto(AccountConstants.STATUS_500, AccountConstants.MESSAGE_500));
+    }
+
     @GetMapping
     public ResponseEntity<CustomerDto> fetchAccountDetailsByMobileNumber(@RequestParam String mobileNumber) {
         return ResponseEntity.ok(accountService.fetchAccountByMobileNumber(mobileNumber));
