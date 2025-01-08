0d0b12957a767aa0c1a797d3df689a2525f7e88b
Tutku Ince <tutku.ince@outlook.com>
2025-01-08 19:32:24 +0300

    Enhanced documentation of REST APIs for DTOs

--- account-service/src/main/java/com/incetutku/accountservice/dto/AccountDto.java
+++ account-service/src/main/java/com/incetutku/accountservice/dto/AccountDto.java
@@ -1,18 +1,35 @@
 package com.incetutku.accountservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Account",
+        description = "Schema to hold Account information"
+)
 public class AccountDto {
+
+    @Schema(
+            description = "Account Number of Easy Bank Account"
+    )
     @NotEmpty(message = "Account Number cannot be null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "mobile number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of Easy Bank Account",
+            example = "Savings"
+    )
     @NotEmpty(message = "Account Type cannot be a null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Easy Bank branch address",
+            example = "123 Izmir"
+    )
     @NotEmpty(message = "Branch Address cannot be a null or empty")
     private String branchAddress;
 }
--- account-service/src/main/java/com/incetutku/accountservice/dto/CustomerDto.java
+++ account-service/src/main/java/com/incetutku/accountservice/dto/CustomerDto.java
@@ -1,22 +1,43 @@
 package com.incetutku.accountservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
+    @Schema(
+            description = "Name of the Customer",
+            example = "Tutku Ince"
+    )
     @NotEmpty(message = "Name cannot be null or empty")
     private String name;
 
+    @Schema(
+            description = "Email of the Customer",
+            example = "tince@mail.com"
+    )
     @NotEmpty(message = "Email Address cannot be null or empty")
     @Email(message = "Email Address should be a valid value")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the Customer",
+            example = "5555555555"
+    )
     @NotEmpty(message = "Mobile Number cannot be null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "mobile number must be 10 digits")
     private String mobileNumber;
+
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountDto accountDto;
 }
