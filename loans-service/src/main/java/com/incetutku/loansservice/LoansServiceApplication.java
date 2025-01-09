1f9624cdf21e2b0bdb270019522bc0fc38532981
Tutku Ince <tutku.ince@outlook.com>
2025-01-09 19:16:05 +0300

    auditing

--- loans-service/src/main/java/com/incetutku/loansservice/LoansServiceApplication.java
+++ loans-service/src/main/java/com/incetutku/loansservice/LoansServiceApplication.java
@@ -2,8 +2,10 @@ package com.incetutku.loansservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansServiceApplication {
 
     public static void main(String[] args) {
