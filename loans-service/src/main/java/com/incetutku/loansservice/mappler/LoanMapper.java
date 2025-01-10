41535a608573a0dff16917d50b3d13c607a719d2
Tutku Ince <tutku.ince@outlook.com>
2025-01-10 21:11:12 +0300

    mapper

---
+++ loans-service/src/main/java/com/incetutku/loansservice/mappler/LoanMapper.java
@@ -0,0 +1,27 @@
+package com.incetutku.loansservice.mappler;
+
+import com.incetutku.loansservice.dto.LoanDto;
+import com.incetutku.loansservice.entity.Loan;
+
+public class LoanMapper {
+
+    public static LoanDto mapToLoanDto(Loan loan, LoanDto loanDto) {
+        loanDto.setLoanNumber(loan.getLoanNumber());
+        loanDto.setLoanType(loan.getLoanType());
+        loanDto.setMobileNumber(loan.getMobileNumber());
+        loanDto.setTotalLoan(loan.getTotalLoan());
+        loanDto.setAmountPaid(loan.getAmountPaid());
+        loanDto.setOutstandingAmount(loan.getOutstandingAmount());
+        return loanDto;
+    }
+
+    public static Loan mapToLoan(LoanDto loanDto, Loan loan) {
+        loan.setLoanNumber(loanDto.getLoanNumber());
+        loan.setLoanType(loanDto.getLoanType());
+        loan.setMobileNumber(loanDto.getMobileNumber());
+        loan.setTotalLoan(loanDto.getTotalLoan());
+        loan.setAmountPaid(loanDto.getAmountPaid());
+        loan.setOutstandingAmount(loanDto.getOutstandingAmount());
+        return loan;
+    }
+}
