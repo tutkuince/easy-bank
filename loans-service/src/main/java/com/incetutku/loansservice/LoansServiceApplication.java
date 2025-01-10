ea5b0ce70663b62dbce65a5b609b8cb68967ec3d
Tutku Ince <tutku.ince@outlook.com>
2025-01-10 21:22:09 +0300

    Documented

--- loans-service/src/main/java/com/incetutku/loansservice/LoansServiceApplication.java
+++ loans-service/src/main/java/com/incetutku/loansservice/LoansServiceApplication.java
@@ -1,11 +1,36 @@
 package com.incetutku.loansservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loan Microservice REST API Documentation",
+                description = "EasyBank Loan Microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Tutku Ince",
+                        email = "tutku.ince@outlook.com",
+                        url = "https://github.com/tutkuince"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/tutkuince"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EasyBank Account Service REST API Documentation",
+                url = "http://localhost:8080/swagger-ui/index.html"
+        )
+)
 public class LoansServiceApplication {
 
     public static void main(String[] args) {
