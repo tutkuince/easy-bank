09db7d19b3f07743330d67ae972995d8d48db879
Tutku Ince <tutku.ince@outlook.com>
2025-01-10 21:21:59 +0300

    updated

--- loans-service/src/main/java/com/incetutku/loansservice/audit/AuditAwareImpl.java
+++ loans-service/src/main/java/com/incetutku/loansservice/audit/AuditAwareImpl.java
@@ -10,6 +10,6 @@ public class AuditAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("ACCOUNT_SERVICE");
+        return Optional.of("LOAN_SERVICE");
     }
 }
