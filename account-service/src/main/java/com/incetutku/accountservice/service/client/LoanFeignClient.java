42608a290d3567d54f972e581dc4d098adce1ad0
Tutku Ince <tutku.ince@outlook.com>
2025-01-31 22:11:24 +0300

    name has been changed - bug fix

--- account-service/src/main/java/com/incetutku/accountservice/service/client/LoanFeignClient.java
+++ account-service/src/main/java/com/incetutku/accountservice/service/client/LoanFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("loan-service")
+@FeignClient("loans-service")
 public interface LoanFeignClient {
 
     @GetMapping(value = "/api/v1/loans", consumes = "application/json")
