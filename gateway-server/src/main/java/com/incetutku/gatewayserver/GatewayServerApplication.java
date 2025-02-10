692a6434c2e4cd557a984e5593c08b5588461aa8
Tutku Ince <tutku.ince@outlook.com>
2025-02-10 18:24:18 +0300

    configuration

--- gateway-server/src/main/java/com/incetutku/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/incetutku/gatewayserver/GatewayServerApplication.java
@@ -7,10 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -26,21 +29,22 @@ public class GatewayServerApplication {
     public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(p -> p.path("/easybank/account-service/**")
-                        .filters(f -> f.rewritePath("/easybank/accounts/(?<segment>.*)","/${segment}")
+                        .filters(f -> f.rewritePath("/easybank/accounts/(?<segment>.*)", "/${segment}")
                                 .addRequestHeader("X-Response-Time", LocalDateTime.now().toString())
                                 .circuitBreaker(config -> config.setName("accountCircuitBreaker")
                                         .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNT-SERVICE"))
                 .route(p -> p.path("/easybank/card-service/**")
-                        .filters(f -> f.rewritePath("/easybank/card-service/(?<segment>.*)","/${segment}")
+                        .filters(f -> f.rewritePath("/easybank/card-service/(?<segment>.*)", "/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(userKeyResolver())))
+                        .uri("lb://CARD-SERVICE"))
+                .route(p -> p.path("/easybank/loans-service/**")
+                        .filters(f -> f.rewritePath("/easybank/loans-service/(?<segment>.*)", "/${segment}")
                                 .addRequestHeader("X-Response-Time", LocalDateTime.now().toString())
                                 .retry(retryConfig -> retryConfig.setRetries(3)
                                         .setMethods(HttpMethod.GET)
                                         .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
-                        .uri("lb://CARD-SERVICE"))
-                .route(p -> p.path("/easybank/loans-service/**")
-                        .filters(f -> f.rewritePath("/easybank/loans-service/(?<segment>.*)","/${segment}")
-                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://ACCOUNT-SERVICE"))
                 .build();
     }
@@ -50,5 +54,16 @@ public class GatewayServerApplication {
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
                 .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
                 .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+                .defaultIfEmpty("anonymous");
     }
 }
